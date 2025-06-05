c0b0263af7a5fe7c1b4c76d47846998abb90fbdb
alexsandro83 <alexsander83@hotmail.com>
2025-06-05 15:25:47 -0300

    atualização do menu para dispositivos menores

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="py-6 px-1 md:px-8 bg-gradient-to-r from-accessibility-main to-accessibility-accent text-white">
       <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+        <div className="flex flex-col md:flex-col items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             
             <Accessibility size={40} />
@@ -18,7 +18,7 @@ const Header = () => {
             <section id="#">
             <p>
               {/* <span className="flex flex-col items-center font-bold ml:px-12">Menu</span> */}
-            <ul className="flex flex-col md:flex-row items-center justify-between gap-4">
+            <ul className="flex flex-row md:flex-row items-center justify-between m-3 gap-4">
               <a href="#"><li>Home</li></a>
               <a href="#Ranking"><li>Ranking</li></a>
               <a href="#Contatos"><li>Contatos</li></a>
